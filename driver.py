d7ed4f9be3278552f8adbb2b00e583886dd82654
kojiboji <kojiboji@gmail.com>
2020-12-18 04:19:41 -0800

    add additional info to profile

--- driver.py
+++ driver.py
@@ -17,6 +17,13 @@ if __name__ == "__main__":
 
   h = hpy()
 
+  setup_start = 0
+  setup_end = 0
+  match_start = 0
+  match_end = 0
+  base_memory = 0
+  total_memory = 0
+
   genome = [random.choice("ACTG") for i in range(gen_len)]
   genome.append("$")
   reads = []
@@ -24,18 +31,19 @@ if __name__ == "__main__":
     j = random.randint(0, len(genome)-y-1)
     reads.append((genome[j:j+y], y))
 
-  start_time = time.perf_counter()
-  end_time = 0
-  memory_usage = 0
-
+  base_memory = h.heap().size
+  
+  setup_start = time.perf_counter()
   sa = suffix_array.make_suffix_array(genome)
 
   if which == "sa":
     flo = []
+    setup_end = time.perf_counter()
+    match_start = time.perf_counter()
     for i in range(x):
       flo.append(suffix_array.pattern_matching_with_suffix_array(genome, reads[i][0], sa))
-    end_time = time.perf_counter()
-    memory_usage = h.heap().size
+    match_end = time.perf_counter()
+    total_memory = h.heap().size
     
     if check:
       for i in range(x):
@@ -49,13 +57,15 @@ if __name__ == "__main__":
   elif which == "bwt":
     n_matches = []
     (fo, bwt, count) = burrows_wheeler_transform.make_everyting(genome, sa)
+    setup_end = time.perf_counter()
     del genome
     del sa
+    match_start = time.perf_counter()
     for i in range(x):
       n_matches.append(burrows_wheeler_transform.better_bw_matching(fo, bwt, reads[i][0], count))
     
-    end_time = time.perf_counter()
-    memory_usage = h.heap().size
+    match_end = time.perf_counter()
+    total_memory = h.heap().size
     
     if check:
       for i in range(x):
@@ -65,7 +75,8 @@ if __name__ == "__main__":
   else:
     raise ValueError("Yo, use 'sa' or 'bwt' to specify the tpye of pattern matching you want")
   
-  exec_time = end_time - start_time
-  print(which, gen_len, x, y, exec_time, memory_usage, sep=", ")
+  setup_time = setup_end - setup_start
+  match_time = match_end - match_start
+  print(which, gen_len, x, y, setup_time, match_time, base_memory, total_memory, sep=", ")
 
   
--- profile.sh
+++ profile.sh
@@ -1,12 +1,12 @@
-echo 'type, gen_len, x(num_reads, y(read_length), time, memory' > $1.csv
+echo 'type, gen_len, x(num_reads, y(read_length), setup_time, match_time, base_memory, setup_memory' > $1.csv
 python3 driver.py $1 1000000 10000 100 False >> $1.csv
 
-for (( y=1; y<=128; y*=2))
+for (( y=1; y<=2**10; y*=2))
 do
   python3 driver.py $1 1000000 10000 $y False >> $1.csv
 done
 
-for (( x=1; x<=131072; x*=2))
+for (( x=1; x<=2**20; x*=2))
 do
   python3 driver.py $1 1000000 $x 100 False >> $1.csv
 done
