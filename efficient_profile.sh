82deb5760f9c30a2cbfecdbdd78648a9dc17a1bb
kojiboji <kojiboji@gmail.com>
2020-12-18 09:17:06 -0800

    get rid of the extraneous call

--- efficient_profile.sh
+++ efficient_profile.sh
@@ -1,5 +1,4 @@
 echo 'type, gen_len, x(num_reads, y(read_length), setup_time, match_time, base_memory, setup_memory' > ebwt.csv
-python3 driver.py ebwt 1000000 10000 100 False >> ebwt.csv
 
 for (( k=1; k<=2**7; k*=2))
 do
